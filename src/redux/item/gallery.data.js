2a604e117e2acf1226f0cb397eeac4b2e6cd3e1b
Hee Su <kessris@hotmail.com>
2019-12-06 15:24:00 +0900

    added unique key to items

--- src/redux/item/gallery.data.js
+++ src/redux/item/gallery.data.js
@@ -61,434 +61,434 @@ const GALLERY_DATA = {
             imageUrl: 'https://i.ibb.co/6yYM0GJ/Untitled232.jpg'
         },
         {
-            id: 10,
+            id: 12,
             name: 'Assorted Meringue Cookies',
             imageUrl: 'https://i.ibb.co/QcTSLDP/Untitled23.jpg'
         },
         {
-            id: 10,
+            id: 13,
             name: 'Earl Grey, Matcha, Coffee Macarons',
             imageUrl: 'https://i.ibb.co/nQxjJqV/20180924-150533.jpg'
         },
         {
-            id: 10,
+            id: 14,
             name: 'Mini Coffee Macarons',
             imageUrl: 'https://i.ibb.co/5FkWy5T/Untitled.jpg'
         },
         {
-            id: 10,
+            id: 15,
             name: 'Orange Sparkling Lemonade',
             imageUrl: 'https://i.ibb.co/G2FqrdD/20190422-195607.jpg'
         },
         {
-            id: 10,
+            id: 16,
             name: 'Korean Lunch in the Patio',
             imageUrl: 'https://i.ibb.co/f92cWz6/20190415-182022.jpg'
         },
         {
-            id: 10,
+            id: 17,
             name: '새우덮밥',
             imageUrl: 'https://i.ibb.co/JsYVhdw/20190316-143411.jpg'
         },
         {
-            id: 10,
+            id: 18,
             name: 'Carbonara Spaghetti',
             imageUrl: 'https://i.ibb.co/z85MsNJ/20190217-190133.jpg'
         },
         {
-            id: 10,
+            id: 19,
             name: 'Coffee and Snacks',
             imageUrl: 'https://i.ibb.co/hfQWYxf/20190112-153754.jpg'
         },
         {
-            id: 10,
+            id: 20,
             name: 'Christmas Dinner cooked Alone',
             imageUrl: 'https://i.ibb.co/4VnYjc8/20181225-193059.jpg'
         },
         {
-            id: 10,
+            id: 21,
             name: 'Christmas Dinner cooked alone',
             imageUrl: 'https://i.ibb.co/y6kSknD/20181225-193050.jpg'
         },
         {
-            id: 10,
+            id: 22,
             name: 'Christmas Dinner cooked alone',
             imageUrl: 'https://i.ibb.co/xhWkrKQ/20181225-193018.jpg'
         },
         {
-            id: 10,
+            id: 23,
             name: 'Pumpkin Macarons',
             imageUrl: 'https://i.ibb.co/7t8cYvv/20181028-212707.jpg'
         },{
-            id: 10,
+            id: 24,
             name: 'Halloween Cookies',
             imageUrl: 'https://i.ibb.co/rFdQv7K/20181028-000140.jpg'
         },
         {
-            id: 10,
+            id: 25,
             name: 'Gift for my friend',
             imageUrl: 'https://i.ibb.co/Ryk0Dbb/20180930-131619.jpg'
         },
         {
-            id: 10,
+            id: 26,
             name: 'Rookie Cookie at SAP (what I brought to my team during my internship)',
             imageUrl: 'https://i.ibb.co/9nHTp0T/20180917-144414.jpg'
         },
         {
-            id: 10,
+            id: 27,
             name: 'Coffee and Mini Bear Macarons',
             imageUrl: 'https://i.ibb.co/7JwBJnG/20180915-224456.jpg'
         },
         {
-            id: 10,
+            id: 28,
             name: 'Soboro Bread',
             imageUrl: 'https://i.ibb.co/djG56z5/20180806-193611.jpg'
         },{
-            id: 10,
+            id: 29,
             name: 'Coffee Bun',
             imageUrl: 'https://i.ibb.co/Wvw2pFG/20180801-215738.jpg'
         },
         {
-            id: 10,
+            id: 30,
             name: 'Coffee Bun Baking Procedure',
             imageUrl: 'https://i.ibb.co/JBL3nMR/20180801-213732.jpg'
         },
         {
-            id: 10,
+            id: 31,
             name: 'Blue Lagoon',
             imageUrl: 'https://i.ibb.co/KsXcLkp/20180617-170915.jpg'
         },
         {
-            id: 10,
+            id: 32,
             name: 'Omelette Rice',
             imageUrl: 'https://i.ibb.co/j4HQdcQ/20180107-003908.jpg'
         },
         {
-            id: 10,
+            id: 33,
             name: 'Mochi Snack with Caramel Dipping Sauce',
             imageUrl: 'https://i.ibb.co/8MNVfQd/20180101-190358.jpg'
         },
         {
-            id: 10,
+            id: 34,
             name: 'Gift to my Co-workers',
             imageUrl: 'https://i.ibb.co/QjB0rwj/20171229-153432.jpg'
         },{
-            id: 10,
+            id: 35,
             name: 'Macarons',
             imageUrl: 'https://i.ibb.co/MGHRzSv/20171229-141943.jpg'
         },
         {
-            id: 10,
+            id: 36,
             name: 'Some kind of sushi I made',
             imageUrl: 'https://i.ibb.co/CQ1VRRV/20171212-153936.jpg'
         },
         {
-            id: 10,
+            id: 37,
             name: 'Marble Macarons',
             imageUrl: 'https://i.ibb.co/C04XgZq/20170828-191046.jpg'
         },{
-            id: 10,
+            id: 38,
             name: 'Macaron baking process',
             imageUrl: 'https://i.ibb.co/YLCcqkz/20170827-191118.jpg'
         },{
-            id: 10,
+            id: 39,
             name: `Gifts to my mom's friends`,
             imageUrl: 'https://i.ibb.co/PD4vKnq/20170719-140138.jpg'
         },
         {
-            id: 10,
+            id: 40,
             name: `Gifts to my mom's friends`,
             imageUrl: 'https://i.ibb.co/GxLnNSR/20170719-140106.jpg'
         },
         {
-            id: 10,
+            id: 41,
             name: 'Mini Bear Macarons',
             imageUrl: 'https://i.ibb.co/7KqLb8B/20170718-230509.jpg'
         },{
-            id: 10,
+            id: 42,
             name: 'Mochi Snacks with Caramel Dipping Sauce',
             imageUrl: 'https://i.ibb.co/Ry0VbsN/20170713-222415.jpg'
         },
         {
-            id: 10,
+            id: 43,
             name: 'Cookie Choux',
             imageUrl: 'https://i.ibb.co/1XY54DK/20170701-165938.jpg'
         },
         {
-            id: 10,
+            id: 44,
             name: 'Chocolate Macarons',
             imageUrl: 'https://i.ibb.co/HXX5T4r/20170613-181911.jpg'
         },
         {
-            id: 10,
+            id: 45,
             name: 'Flower Cookies',
             imageUrl: 'https://i.ibb.co/X3YB0CM/20170530-201751.jpg'
         },
         {
-            id: 10,
+            id: 46,
             name: 'Pecan Pie',
             imageUrl: 'https://i.ibb.co/1T6F2KL/20170528-190014.jpg'
         },
         {
-            id: 10,
+            id: 47,
             name: 'Chocolate Macaron baking process',
             imageUrl: 'https://i.ibb.co/R4vH2Dw/20170524-183624.jpg'
         },
         {
-            id: 10,
+            id: 48,
             name: 'Japanese Soba',
             imageUrl: 'https://i.ibb.co/n0CxRzB/20170514-133027.jpg'
         },
         {
-            id: 10,
+            id: 49,
             name: `Breakfast for my mom on Mother's Day`,
             imageUrl: 'https://i.ibb.co/Sd2w4R2/20170514-081542.jpg'
         },
         {
-            id: 10,
+            id: 50,
             name: 'Yangang with Sunflower seeds',
             imageUrl: 'https://i.ibb.co/nRd6bP0/20170504-151958.jpg'
         },
         {
-            id: 10,
+            id: 51,
             name: 'Yangang with Sunflower seeds',
             imageUrl: 'https://i.ibb.co/VW8yGm0/20170504-151905.jpg'
         },
         {
-            id: 10,
+            id: 52,
             name: 'Colourful Layered Yangang',
             imageUrl: 'https://i.ibb.co/v1B6MsG/20170503-161249.jpg'
         },
         {
-            id: 10,
+            id: 53,
             name: 'Colourful Layered Yangang',
             imageUrl: 'https://i.ibb.co/jbLRzGy/20170503-161158.jpg'
         },
         {
-            id: 10,
+            id: 54,
             name: 'Chocolate Buttercream Macarons',
             imageUrl: 'https://i.ibb.co/Smyx5X1/20170421-161646.jpg'
         },
         {
-            id: 10,
+            id: 55,
             name: 'Chocolate Buttercream Macarons',
             imageUrl: 'https://i.ibb.co/0Xjfdbw/20170421-161200.jpg'
         },
         {
-            id: 10,
+            id: 56,
             name: 'Chocolate Macaron baking process',
             imageUrl: 'https://i.ibb.co/5K0xKYf/20170421-151521.jpg'
         },
         {
-            id: 10,
+            id: 57,
             name: 'Gift for my friends',
             imageUrl: 'https://i.ibb.co/6v5Q2vF/20170420-105813.jpg'
         },
         {
-            id: 10,
+            id: 58,
             name: 'Gift for my friends',
             imageUrl: 'https://i.ibb.co/PmYP12t/20170420-105743.jpg'
         },
         {
-            id: 10,
+            id: 59,
             name: 'Gift for my friends',
             imageUrl: 'https://i.ibb.co/hD5q74J/20170420-105038.jpg'
         },{
-            id: 10,
+            id: 60,
             name: 'Mini Bear Macarons',
             imageUrl: 'https://i.ibb.co/RT3ZT3X/20170420-104539.jpg'
         },
         {
-            id: 10,
+            id: 61,
             name: 'Mini Emoji Macarons',
             imageUrl: 'https://i.ibb.co/MDv4cPJ/20170420-104531.jpg'
         },
         {
-            id: 10,
+            id: 62,
             name: 'Mini Emoji Macarons',
             imageUrl: 'https://i.ibb.co/GQCZkdz/20170420-104519.jpg'
         },{
-            id: 10,
+            id: 63,
             name: 'Gift for my club members (UKHAN)',
             imageUrl: 'https://i.ibb.co/YPCBZd2/20170406-144125.jpg'
         },
         {
-            id: 10,
+            id: 64,
             name: 'Gift for my club members (UKHAN)',
             imageUrl: 'https://i.ibb.co/HY4qPvd/20170406-143956.jpg'
         },
         {
-            id: 10,
+            id: 65,
             name: 'Coffee Macarons',
             imageUrl: 'https://i.ibb.co/mv5pW1X/20170308-074651.jpg'
         },
         {
-            id: 10,
+            id: 66,
             name: 'Mini Bear Macarons',
             imageUrl: 'https://i.ibb.co/K95DP9J/20170308-074622.jpg'
         },
         {
-            id: 10,
+            id: 67,
             name: 'Mini Bear Macarons',
             imageUrl: 'https://i.ibb.co/9bHPg8w/20170308-074555.jpg'
         },{
-            id: 10,
+            id: 68,
             name: 'Stacks of Macarons',
             imageUrl: 'https://i.ibb.co/C6Mbt1C/20170307-183826.jpg'
         },{
-            id: 10,
+            id: 69,
             name: 'Character Macaron',
             imageUrl: 'https://i.ibb.co/J2CMfJq/20170307-182549.jpg'
         },
         {
-            id: 10,
+            id: 70,
             name: 'Coffee and Caramel Macarons',
             imageUrl: 'https://i.ibb.co/mqcVvjy/20170307-181725.jpg'
         },{
-            id: 10,
+            id: 71,
             name: 'Coffee and Caramel Macarons',
             imageUrl: 'https://i.ibb.co/gTmTdxZ/20170307-180431.jpg'
         },
         {
-            id: 10,
+            id: 72,
             name: `Gift for my mom's friends`,
             imageUrl: 'https://i.ibb.co/5kRJCVv/20170304-130111.jpg'
         },
         {
-            id: 10,
+            id: 73,
             name: 'Green Tea and Earl Grey Flower Macarons',
             imageUrl: 'https://i.ibb.co/MkxMqyk/20170304-122516.jpg'
         },
         {
-            id: 10,
+            id: 74,
             name: 'Earl Grey Macarons',
             imageUrl: 'https://i.ibb.co/JHpFcMt/20170304-123020.jpg'
         },
         {
-            id: 10,
+            id: 75,
             name: 'Flower Macarons',
             imageUrl: 'https://i.ibb.co/0nSTzQ2/20170304-121359.jpg'
         },
         {
-            id: 10,
+            id: 76,
             name: 'Gift for my club members (UKHAN)',
             imageUrl: 'https://i.ibb.co/mhbBGnY/20170206-120628.jpg'
         },
         {
-            id: 10,
+            id: 77,
             name: 'Gift for my club members (UKHAN)',
             imageUrl: 'https://i.ibb.co/vH5KhSx/20170204-151836.jpg'
         },
         {
-            id: 10,
+            id: 78,
             name: 'Eggs in Hell',
             imageUrl: 'https://i.ibb.co/44hjhcQ/20170201-165704.jpg'
         },
         {
-            id: 10,
+            id: 79,
             name: 'Snack for my club members (UKHAN)',
             imageUrl: 'https://i.ibb.co/nzzFbX4/20170129-145240.jpg'
         },
         {
-            id: 10,
+            id: 80,
             name: 'Snack for my club members (UKHAN)',
             imageUrl: 'https://i.ibb.co/CtZCZQp/20170129-145228.jpg'
         },
         {
-            id: 10,
+            id: 81,
             name: 'Chicken and Chick Meringue Cookies',
             imageUrl: 'https://i.ibb.co/WP2dsvd/20170107-232704.jpg'
         },{
-            id: 10,
+            id: 82,
             name: 'Chicken and Chick Meringue Cookies',
             imageUrl: 'https://i.ibb.co/17Cd7K1/20170107-210254.jpg'
         },
         {
-            id: 10,
+            id: 83,
             name: 'Chicken and Chick Meringue Cookies',
             imageUrl: 'https://i.ibb.co/ckQvbvM/20170107-210214.jpg'
         },
         {
-            id: 10,
+            id: 84,
             name: 'Christmas gifts',
             imageUrl: 'https://i.ibb.co/RpKvgc6/20161222-131906.jpg'
         },
         {
-            id: 10,
+            id: 85,
             name: 'Christmas Gifts',
             imageUrl: 'https://i.ibb.co/HX4C9Rc/20161222-131859.jpg'
         },
         {
-            id: 10,
+            id: 86,
             name: 'Ryan Macarons',
             imageUrl: 'https://i.ibb.co/gRczHN5/20161221-180858.jpg'
         },
         {
-            id: 10,
+            id: 87,
             name: 'Ryan Macarons',
             imageUrl: 'https://i.ibb.co/sgrB9FZ/20161221-180816.jpg'
         },
         {
-            id: 10,
+            id: 88,
             name: '미니김밥',
             imageUrl: 'https://i.ibb.co/gDpqF3h/20160827-184118.jpg'
         },
         {
-            id: 10,
+            id: 89,
             name: '덮밥',
             imageUrl: 'https://i.ibb.co/Z84zvLd/20160825-181826.jpg'
         },{
-            id: 10,
+            id: 90,
             name: 'Butter Chicken',
             imageUrl: 'https://i.ibb.co/9vZKXMn/20160808-204326.jpg'
         },
         {
-            id: 10,
+            id: 91,
             name: 'Yaki Udon',
             imageUrl: 'https://i.ibb.co/D8VGvDR/20160807-192842.jpg'
         },
         {
-            id: 10,
+            id: 92,
             name: 'Yaki Udon',
             imageUrl: 'https://i.ibb.co/7CFdSKj/20160728-205839.jpg'
         },
         {
-            id: 10,
+            id: 93,
             name: '콩국수',
             imageUrl: 'https://i.ibb.co/Wtr31JQ/20160726-135348.jpg'
         },
         {
-            id: 10,
+            id: 94,
             name: '새우덮밥',
             imageUrl: 'https://i.ibb.co/VYSCXKn/20160708-141331.jpg'
         },
         {
-            id: 10,
+            id: 95,
             name: '볶음밥 Fried Rice',
             imageUrl: 'https://i.ibb.co/pvXBbZp/20160707-080813.jpg'
         },
         {
-            id: 10,
+            id: 96,
             name: 'Green Tea Almond Cookies',
             imageUrl: 'https://i.ibb.co/XV3cFNN/20160216-215451.jpg'
         },
         {
-            id: 10,
+            id: 97,
             name: 'Green Tea Almond Cookies and Earl Grey Cookies',
             imageUrl: 'https://i.ibb.co/k6HHV8m/20160216-215425.jpg'
         },
         {
-            id: 10,
+            id: 98,
             name: 'Earl Grey Cookies',
             imageUrl: 'https://i.ibb.co/PjhFJmz/20160216-184207.jpg'
         },
         {
-            id: 10,
+            id: 99,
             name: 'Christmas Icing Cookies',
             imageUrl: 'https://i.ibb.co/PgYQwSz/20151224-225759.jpg'
         },
         {
-            id: 10,
+            id: 100,
             name: 'Alfredo Pasta',
             imageUrl: 'https://i.ibb.co/7NXzcZY/20151229-192632-1.jpg'
         }
--- src/redux/item/item.data.js
+++ src/redux/item/item.data.js
@@ -27,80 +27,80 @@ const ITEM_DATA = [
         routeName: 'macarons',
         items: [
             {
-                id: 10,
+                id: 3,
                 name: 'Ryan Macaron [x30]',
                 imageUrl: 'https://i.ibb.co/sgrB9FZ/20161221-180816.jpg',
                 price: 60
             },
             {
-                id: 11,
+                id: 4,
                 name: 'Mini Emoji Macaron [x40]',
                 imageUrl: 'https://i.ibb.co/MDv4cPJ/20170420-104531.jpg',
                 price: 35
             },
             {
-                id: 12,
+                id: 5,
                 name: 'Flower Macaron Set (Earl Grey & Matcha) [x30]',
                 imageUrl: 'https://i.ibb.co/bzfZsL8/1234.jpg',
                 price: 90
             },
             {
-                id: 1423,
+                id: 6,
                 name: 'Mini Coffee Macaron [x40]',
                 imageUrl: 'https://i.ibb.co/KwDjk96/Untitled.jpg',
                 price: 35
             },
             {
-                id: 13,
+                id: 7,
                 name: 'Mini Bear Macaron [x40]',
                 imageUrl: 'https://i.ibb.co/RT3ZT3X/20170420-104539.jpg',
                 price: 45
             },
             {
-                id: 133,
+                id: 8,
                 name: 'Mini Bear2 Macaron [x40]',
                 imageUrl: 'https://i.ibb.co/T2bGFTT/20170307-182301.jpg',
                 price: 35
             },
             {
-                id: 14,
+                id: 9,
                 name: 'Pumpkin Macaron [x30]',
                 imageUrl: 'https://i.ibb.co/7t8cYvv/20181028-212707.jpg',
                 price: 37
             },
             {
-                id: 146,
+                id: 10,
                 name: 'Matcha Flower Macaron [x30]',
                 imageUrl: 'https://i.ibb.co/Gn23yLz/43.jpg',
                 price: 65
             },
             {
-                id: 143,
+                id: 11,
                 name: 'Marble Macaron [x30]',
                 imageUrl: 'https://i.ibb.co/ft4Q6p9/Untitled2325.jpg',
                 price: 45
             },
             {
-                id: 15,
+                id: 12,
                 name: 'Coffee Macaron [x30]',
                 imageUrl: 'https://i.ibb.co/724mQfy/Untitled.jpg',
                 price: 40
             },
             {
-                id: 151,
+                id: 13,
                 name: 'Earl Grey Macaron [x30]',
                 imageUrl: 'https://i.ibb.co/MhCNqnt/12.jpg',
                 price: 40
             },
             {
-                id: 177,
+                id: 14,
                 name: 'Chocolate Macaron [x30]',
                 // imageUrl: 'https://i.ibb.co/Mhh6wBg/timberlands.png',
                 imageUrl: "https://i.ibb.co/Smyx5X1/20170421-161646.jpg",
                 price: 40
             },
             {
-                id: 16,
+                id: 15,
                 name: 'Vanilla and Chocolate Macaron Set [x30]',
                 imageUrl: 'https://i.ibb.co/RBKCXbF/20171229-141943.jpg',
                 price: 45
@@ -119,7 +119,7 @@ const ITEM_DATA = [
                 price: 160
             },
             {
-                id: 1737,
+                id: 18,
                 name: 'Assorted Macaron [x30]',
                 // imageUrl: 'https://i.ibb.co/Mhh6wBg/timberlands.png',
                 imageUrl: "https://i.ibb.co/p4YdF6s/20180924-150533.jpg",
@@ -133,49 +133,49 @@ const ITEM_DATA = [
         routeName: 'cookies',
         items: [
             {
-                id: 18,
+                id: 19,
                 name: 'Green Tea Almond Cookie [x40]',
                 imageUrl: 'https://i.ibb.co/XV3cFNN/20160216-215451.jpg',
                 price: 30
             },
             {
-                id: 19,
+                id: 20,
                 name: 'Flower Cookie [x30]',
                 imageUrl: 'https://i.ibb.co/dt96Jgn/20170530-202853.jpg',
                 price: 60
             },
             {
-                id: 20,
+                id: 21,
                 name: 'Chicken Meringue Cookie [x50]',
                 imageUrl: 'https://i.ibb.co/ckQvbvM/20170107-210214.jpg',
                 price: 35
             },
             {
-                id: 20,
+                id: 22,
                 name: 'Assorted Meringue Cookie [x50]',
                 imageUrl: 'https://i.ibb.co/8BfvqYr/Untitled23.jpg',
                 price: 25
             },
             {
-                id: 21,
+                id: 23,
                 name: 'Halloween Icing Cookies [x30]',
                 imageUrl: 'https://i.ibb.co/rFdQv7K/20181028-000140.jpg',
                 price: 50
             },
             {
-                id: 2224,
+                id: 24,
                 name: 'Chocolate Almond Biscottis [x40]',
                 imageUrl: 'https://i.ibb.co/2F25qsv/b.jpg',
                 price: 35
             },
             {
-                id: 22,
+                id: 25,
                 name: 'Christmas Icing Cookies [x30]',
                 imageUrl: 'https://i.ibb.co/9WvtsM3/20151224-225759.jpg',
                 price: 40
             },
             {
-                id: 222,
+                id: 26,
                 name: 'Earl Grey Cookies [x30]',
                 imageUrl: 'https://i.ibb.co/tKsQnmP/20160216-184207.jpg',
                 price: 30
@@ -188,19 +188,19 @@ const ITEM_DATA = [
         routeName: 'cakes',
         items: [
             {
-                id: 23,
+                id: 27,
                 name: 'Pound Cake [x3]',
                 imageUrl: 'https://i.ibb.co/bBvHKh4/121.jpg',
                 price: 15
             },
             {
-                id: 24,
+                id: 28,
                 name: 'Japanese Cheesecake [x3]',
                 imageUrl: 'https://i.ibb.co/6yYM0GJ/Untitled232.jpg',
                 price: 30
             },
             {
-                id: 25,
+                id: 29,
                 name: 'Matcha Crepe Cake [x3]',
                 imageUrl: 'https://i.ibb.co/C16SyX0/20190302-110455.jpg',
                 price: 60
