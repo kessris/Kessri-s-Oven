cec715a9ae1cfe4882ae0aa9da4977e52cefb680
Hee Su <kessris@hotmail.com>
2019-12-08 18:58:32 +0900

    how-to-order, qna, contact pages' mobile layout implemented

--- src/pages/how-to-order/how-to-order.component.jsx
+++ src/pages/how-to-order/how-to-order.component.jsx
@@ -4,11 +4,13 @@ import './how-to-order.styles.scss';
 const HowToOrder = () => (
     <div className='how-to-order-page'>
         <h1 className='title'>How To Order</h1>
-        <h2>** PLEASE DO NOT TRY TO ORDER ANYTHING ** </h2>
-        <h2 style={{color: 'red'}}>**ORDERING WILL NOT WORK **</h2>
-        <p>
-            THIS IS A MOCK E-COMMERCE WEBSITE BUILT AS A PERSONAL PROJECT.
-        </p>
+        <div className='content'>
+            <h2>** PLEASE DO NOT TRY TO ORDER ANYTHING ** </h2>
+            <h2 style={{color: 'red'}}>**ORDERING WILL NOT WORK **</h2>
+            <p>
+                THIS IS A MOCK E-COMMERCE WEBSITE BUILT AS A PERSONAL PROJECT.
+            </p>
+        </div>
     </div>
 );
 
